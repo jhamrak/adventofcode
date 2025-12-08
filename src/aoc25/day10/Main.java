0e28f4eb67a55b143e570d5b9556eb3103bf75da
János Hamrák <janos.hamrak@icellmobilsoft.hu>
2025-12-08 08:42:32 +0100

    Adds template code for AoC days 9-12
    Adds template code for Advent of Code days 9 through 12,
    including Main.java, input.txt, and test.txt files.
    This provides a starting point for solving the puzzles.

---
+++ src/aoc25/day10/Main.java
@@ -0,0 +1,26 @@
+package aoc25.day10;
+
+import common.Helper;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> data = Helper.readLines(Main.class, Helper.TEST);
+        Long result = part1(data);
+//        Long result = part2(data);
+        System.out.println(result);
+    }
+
+    private static Long part1(List<String> data) {
+        Long result = 0L;
+        return result;
+    }
+
+    private static Long part2(List<String> data) {
+        Long result = 0L;
+        return result;
+    }
+
+}
